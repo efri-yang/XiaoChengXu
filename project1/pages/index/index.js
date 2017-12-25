1984abee649f42d2333dbd0f4d57fb1d2495fe7c
efri-yang <948061564@qq.com>
2017-12-25 17:34:33 +0800

    c

--- project1/pages/index/index.js
+++ project1/pages/index/index.js
@@ -1,39 +1,30 @@
 
 const app = getApp();
 
-var order = ['demo1', 'demo2', 'demo3']
+
 Page({
   data: {
-    toView: 'green'
+    arrow: '../../images/h_arrow_1.png',
+    tblHeadFixed:false,
+    navAllShow:false,
+    isLoading:true
   },
-  upper: function (e) {
-    console.log(e)
-  },
-  lower: function (e) {
-    console.log(e)
-  },
-  scroll: function (e) {
-    console.log(e)
-  },
-  scrollToTop: function (e) {
-    this.setAction({
-      scrollTop: 0
-    })
-  },
-  tap: function (e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
+
+  onPageScroll: function (res){
+    if (res.scrollTop > 80){
         this.setData({
-          toView: order[i + 1],
-          scrollTop: (i + 1) * 200
+            tblHeadFixed:true
         })
-        break
-      }
+    }else{
+      this.setData({
+        tblHeadFixed:false
+      })
     }
   },
-  tapMove: function (e) {
-    this.setData({
-      scrollTop: this.data.scrollTop + 10
-    })
+  navMore:function(){
+      this.setData({
+        navAllShow: !this.data.navAllShow
+      })
   }
+  
 })
