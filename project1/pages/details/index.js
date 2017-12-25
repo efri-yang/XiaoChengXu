75d8bdc36dda4cf193182b97081eff5d42a668c3
efri-yang <948061564@qq.com>
2017-12-25 20:01:37 +0800

    c

--- project1/pages/details/index.js
+++ project1/pages/details/index.js
@@ -1,4 +1,22 @@
 
-const app = getApp();
+
+Page({
+  data: {
+    iconGsWhat: '../../images/details/icon_hdwhat.png',
+    iconDialogClose:"../../images/details/icon_dialog_close.png",
+    isIntroDialogShow:false
+  },
+  
+  shopIntroDialog:function () {
+       this.setData({
+         isIntroDialogShow: !this.data.isIntroDialogShow
+       }) 
+  }
+
+});
+
+
+
+
 
 
